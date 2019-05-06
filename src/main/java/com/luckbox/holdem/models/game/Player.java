09091d464e379edd509c5e58887f372a6ebbb7b9
Daniel Seetoh <danielseetoh92@gmail.com>
2019-05-07 00:22:05 +0800

    add some game logic

---
+++ src/main/java/com/luckbox/holdem/models/game/Player.java
@@ -0,0 +1,17 @@
+package com.luckbox.holdem.models.game;
+
+import com.luckbox.holdem.models.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by danielseetoh on 5/7/19.
+ */
+public class Player {
+    public ArrayList<Card> hand;
+    public User user;
+
+    public Player(User user) {
+        this.user = user;
+    }
+}
--- src/main/java/com/luckbox/holdem/models/game/Table.java
+++ src/main/java/com/luckbox/holdem/models/game/Table.java
@@ -1,13 +1,31 @@
 package com.luckbox.holdem.models.game;
 
+import java.util.ArrayList;
+
 /**
  * Created by danielseetoh on 5/5/19.
  */
 public class Table {
 
     public Deck deck;
+    public ArrayList<Player> players;
+    public int maxPlayers;
 
-    public Table() {
+    public Table(int maxPlayers) {
         this.deck = new Deck();
+        this.maxPlayers = maxPlayers;
+    }
+
+    public void addPlayer(Player player) {
+        if (players.size() < maxPlayers) {
+            players.add(player);
+        } else {
+            // TODO: handle error
+            // add logging
+        }
+    }
+
+    public void removePlayer(Player player) {
+        // TODO
     }
 }
