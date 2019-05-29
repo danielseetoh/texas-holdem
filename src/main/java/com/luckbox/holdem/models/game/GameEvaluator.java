e51fabed4b3c74a424f6d43477bdf07a46f023a8
Daniel Seetoh <danielseetoh92@gmail.com>
2019-05-29 21:35:39 +0800

    refactor to remove some magic numbers

--- src/main/java/com/luckbox/holdem/models/game/GameEvaluator.java
+++ src/main/java/com/luckbox/holdem/models/game/GameEvaluator.java
@@ -9,8 +9,6 @@ import java.util.List;
  * Created by danielseetoh on 5/7/19.
  */
 public class GameEvaluator {
-    // to refactor and get rid of magic numbers
-
     public List<Player> getWinningPlayers(Player[] players, Card[] communityCards) {
         // get best combo of each player
 
@@ -46,11 +44,13 @@ public class GameEvaluator {
     public static List<Card> getHighestStraightFlush(List<Card> sortedCardsList) {
         List<Card> straightFlushCards = new ArrayList<>();
         Card currentCard = null, prevCard = null;
+        int indexOfSecondCardFromTop = sortedCardsList.size() - 2;
+        int STRAIGHT_FLUSH_MIN_CARDS = 5;
 
         straightFlushCards.add(sortedCardsList.get(sortedCardsList.size() - 1));
 
         // iterate through the cards from top down
-        for (int i = sortedCardsList.size() - 2; i >= 0; i--) {
+        for (int i = indexOfSecondCardFromTop; i >= 0; i--) {
             currentCard = sortedCardsList.get(i);
             prevCard = straightFlushCards.get(0);
             if (currentCard.number == prevCard.number) {
@@ -61,7 +61,7 @@ public class GameEvaluator {
                 currentCard.number.ordinal() == prevCard.number.ordinal() - 1) {
                 straightFlushCards.add(0, currentCard);
             } else {
-                if (straightFlushCards.size() >= 5) {
+                if (straightFlushCards.size() >= STRAIGHT_FLUSH_MIN_CARDS) {
                     break;
                 }
                 straightFlushCards.clear();
@@ -70,7 +70,7 @@ public class GameEvaluator {
         }
 
         // handle a wheel (e.g. ace to five)
-        if (straightFlushCards.size() == 4) {
+        if (straightFlushCards.size() == STRAIGHT_FLUSH_MIN_CARDS - 1) {
             currentCard = straightFlushCards.get(0);
             Card aceWheelCard = new Card(CardNumber.ace, currentCard.suit);
             if (currentCard.number == CardNumber.two && sortedCardsList.contains(aceWheelCard)) {
@@ -80,8 +80,8 @@ public class GameEvaluator {
 
         int straightFlushNumCards = straightFlushCards.size();
 
-        if (straightFlushNumCards >= 5) {
-            return straightFlushCards.subList(straightFlushNumCards - 5, straightFlushNumCards);
+        if (straightFlushNumCards >= STRAIGHT_FLUSH_MIN_CARDS) {
+            return straightFlushCards.subList(straightFlushNumCards - STRAIGHT_FLUSH_MIN_CARDS, straightFlushNumCards);
         }
 
         return null;
@@ -89,6 +89,7 @@ public class GameEvaluator {
     
     public static List<Card> getQuads(List<Card> sortedCardsList) {
         int consecutiveSameNumber = 1;
+        int QUADS_NUM_CARDS = 4;
         Card prevCard = sortedCardsList.get(0), currentCard = null;
         
         for (int i = 1; i < sortedCardsList.size(); i++) {
@@ -98,8 +99,8 @@ public class GameEvaluator {
             } else {
                 consecutiveSameNumber = 1;
             }
-            if (consecutiveSameNumber >= 4) {
-                return sortedCardsList.subList(i - 3, i + 1);
+            if (consecutiveSameNumber >= QUADS_NUM_CARDS) {
+                return sortedCardsList.subList(i - QUADS_NUM_CARDS - 1, i + 1);
             }
             prevCard = currentCard;
         }
